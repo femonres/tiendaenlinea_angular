cd08e35cc8b285c824147d41d4c4dabb1ad1f375
Felipe Montoya R <femonres@gmail.com>
2018-04-08 16:50:35 -0500

    Esquema de la Barra superior y fondo de las paginas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCjaOqexPBVLDSXEQ52O_tnpyNPvJsvpuE',
+    authDomain: 'tienda-online-da3f0.firebaseapp.com',
+    databaseURL: 'https://tienda-online-da3f0.firebaseio.com',
+    projectId: 'tienda-online-da3f0',
+    storageBucket: 'tienda-online-da3f0.appspot.com',
+    messagingSenderId: '123593588463'
+  }
 };
